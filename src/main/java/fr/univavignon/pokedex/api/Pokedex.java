e7c5fcd6db35256e582a949bf46d5a26ec8cc39d
TiKissou <bradley.kissouna@etu.univ-orleans.fr>
2024-11-16 05:17:13 +0100

    Reusing < 0 condition

--- src/main/java/fr/univavignon/pokedex/api/Pokedex.java
+++ src/main/java/fr/univavignon/pokedex/api/Pokedex.java
@@ -29,7 +29,7 @@ public class Pokedex implements IPokedex {
 
     @Override
     public Pokemon getPokemon(int id) throws PokedexException {
-        if (id <= 0 || id >= pokemons.size()) {
+        if (id < 0 || id >= pokemons.size()) {
             throw new PokedexException("Invalid Pokemon ID: " + id);
         }
         return pokemons.get(id);
