5a9e10f484267dfbf52c7c22906f93f1a1a93bea
TiKissou <bradley.kissouna@etu.univ-orleans.fr>
2024-12-08 18:26:53 +0100

    Remove folder path of outputFile

--- src/test/java/fr/univavignon/pokedex/api/IPokedexTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexTest.java
@@ -1,7 +1,6 @@
 package fr.univavignon.pokedex.api;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
