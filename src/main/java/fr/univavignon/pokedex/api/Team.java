988fbec905daeb074d1d74bfda4050f977b87933
BradleyKissouna <bradley.kissouna@etu.univ-orleans.fr>
2024-12-09 00:24:39 +0100

    Not working test again

--- src/main/java/fr/univavignon/pokedex/api/Team.java
+++ src/main/java/fr/univavignon/pokedex/api/Team.java
@@ -6,13 +6,19 @@ package fr.univavignon.pokedex.api;
  */
 public enum Team {
 
-	// Team Mystic, led by Blanche
-	MYSTIC, // The mystic team, known for their calm and collected approach.
+	/**
+	 * Team Mystic.
+	 */
+	MYSTIC,
 
-	// Team Instinct, led by Spark
-	INSTINCT, // The instinct team, known for their trust in their instincts.
-
-	// Team Valor, led by Candela
-	VALOR // The valor team, known for their bravery and strength.
+	/**
+	 * Team Instinct.
+	 */
+	INSTINCT,
+	
+	/**
+	 * Team Valor.
+	 */
+	VALOR
 
 }
